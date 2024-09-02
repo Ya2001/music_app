33ee96f1f5a28214000655987627997e8939b257
Akshay <srivats66@gmail.com>
2024-09-02 11:59:42 +0100

    setup spotify passport access

--- main/index.js
+++ main/index.js
@@ -12,3 +12,27 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
+passport.use(new SpotifyStrategy({
+    clientID: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/spotify/callback"
+  },
+  function(accessToken, refreshToken, expires_in, profile, done) {
+    // Save accessToken, refreshToken, and profile info
+    return done(null, profile);
+  }
+));
+
+app.use(passport.initialize());
+app.get('/auth/spotify', passport.authenticate('spotify'));
+app.get('/auth/spotify/callback',
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect to frontend
+    res.redirect('/dashboard');
+  }
+);
